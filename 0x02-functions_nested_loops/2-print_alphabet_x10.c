e63ac656a87511f089321d2dd064759f9c28f5cd
Mohammed <asmahmohammed008@gmail.com>
2023-06-20 19:05:13 -0700

    my commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that will rpint the alphabet 10 times
+ *
+*/
+void print_alphabet_x10(void)
+{
+	int ch;
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+	}
+	putchar('\n');
+}
