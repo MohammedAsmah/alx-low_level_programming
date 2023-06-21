c56275fc80c7f57224bcb0ce3c7169df86411076
Mohammed <asmahmohammed008@gmail.com>
2023-06-21 15:36:41 -0700

    my commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,7 +5,7 @@
  * Purpose - no hardcode
  *
  * Return: (Success)
-*/
+ */
 int main(void)
 {
 	unsigned long int i;
@@ -16,6 +16,7 @@ int main(void)
 	unsigned long int bef2;
 	unsigned long int aft1;
 	unsigned long int aft2;
+
 	printf("%lu", bef);
 	for (i = 1; i < 91; i++)
 	{
@@ -26,4 +27,16 @@ int main(void)
 	bef1 = (bef / l);
 	bef2 = (bef % l);
 	aft1 = (aft / l);
-	aft2 = (aft %
+	aft2 = (aft % l);
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
+	return (0);
+}
