8e5bf416b8b6b8c3129454b1b969d2c0fbe361ed
Mohammed <asmahmohammed008@gmail.com>
2023-06-20 18:20:49 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,13 @@
  * main - prints _putchar using putchar prototype
  * Return: always 0.
 */
-int mAIN(void)
+int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
+
 	for (ch = 0; ch < 8; ch++)
 	_putchar(str[ch]);
-	_putchar('\n');
+	 _putchar('\n');
 return (0);
 }
