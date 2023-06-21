72886b0297f7b92eea01802a8ae19e606b07a4ce
Mohammed <asmahmohammed008@gmail.com>
2023-06-21 11:42:26 -0700

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,18 @@ void times_table(void)
 
 	for (a = 0; a < 10; a++)
 	{
+		_putchar(48);
 		for (b = 0; b < 10; b++)
 		{
-			timestable = a * b;
-			_putchar(timestable + '0');
 			_putchar(',');
 			_putchar('\n');
+			timestable = a * b;
+			if (timestable < 10)
+				_putchar(' ');
+			else
+				_putchar((timestable / 10) + 48);
+			_putchar((timestable % 10) + 48);
 		}
+	_putchar('\n');
 	}
 }
