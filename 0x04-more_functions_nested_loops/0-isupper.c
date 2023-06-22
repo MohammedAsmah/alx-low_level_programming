b611ea78f8acc72b30b5f4839ec82ef659eaa5f4
Mohammed <asmahmohammed008@gmail.com>
2023-06-22 10:08:59 -0700

    my commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _isupper - check if c is upper
- * @c: input alphabet
+ supper.c* @c: input alphabet
  * Return: return 1 if c is uppercase else return 0.
 */
 int _isupper(int c)
 {
-	if (c >= 65, c <= 90)
+	if (c >= 65; c <= 90)
 		return (1);
 	else
 		return (0);
