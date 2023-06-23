c72088a3310f5ee4cb611709c7cf17d1f41b39a4
Mohammed <asmahmohammed008@gmail.com>
2023-06-23 08:52:36 -0700

    my commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,15 @@
 */
 void more_numbers(void)
 {
-	int a, b, c;
+	int a, b;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			c = b;
 			if (b > 9)
-			{
-				_putchar(1 + 48);
-				c = b % 10;
-			}
-			_putchar(c + 48);
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
